b469c0d3c44c33486cd33bba5648537e7213da18
Phil <pdoh00@gmail.com>
2015-04-04 13:30:32 -0700

    adding src. It was getting ignored by global .gitignore

---
+++ README.md
@@ -0,0 +1,1 @@
+My **Go** Language sandbox
---
+++ src/github.com/pdoh00/hello/hello.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/pdoh00/stringutil"
+)
+
+func main() {
+
+	fmt.Print(stringutil.Reverse("hello, Go Lang"))
+}
